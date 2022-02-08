6aa1936381c1c13a0b460fdc3099f853ff4163e2
benzina <benzina.elmahdi@gmail.com>
2022-02-08 21:10:00 +0100

    FEATURE/PO-I5: user repository test

---
+++ src/test/java/com/tuto/springsecuritytuto/repository/UserRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.tuto.springsecuritytuto.repository;
+
+import com.tuto.springsecuritytuto.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("benzina@gmail.com");
+        user.setPassword("benzina");
+        user.setFirstName("El mahdi");
+        user.setLastName("benzina");
+
+        User savedUser = repository.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+
+    }
+}
