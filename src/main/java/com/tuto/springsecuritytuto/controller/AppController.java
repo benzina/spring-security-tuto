d9b168efaeabd6e8e748ca6f4f9c9cdc7ab079aa
benzina <benzina.elmahdi@gmail.com>
2022-02-08 21:39:29 +0100

    FEATURE/PO-I8:process register

--- src/main/java/com/tuto/springsecuritytuto/controller/AppController.java
+++ src/main/java/com/tuto/springsecuritytuto/controller/AppController.java
@@ -3,9 +3,11 @@ package com.tuto.springsecuritytuto.controller;
 import com.tuto.springsecuritytuto.model.User;
 import com.tuto.springsecuritytuto.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -22,5 +24,16 @@ public class AppController {
         model.addAttribute("user", new User());
 
         return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        return "register_success";
     }
 }
