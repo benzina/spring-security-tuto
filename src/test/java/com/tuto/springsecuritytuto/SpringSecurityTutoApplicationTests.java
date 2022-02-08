3e3e8fce1df64fff2dddae890aabe993596934e1
benzina <benzina.elmahdi@gmail.com>
2022-02-08 20:33:29 +0100

    FEATURE/PO-I1:project structure

---
+++ src/test/java/com/tuto/springsecuritytuto/SpringSecurityTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.springsecuritytuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
